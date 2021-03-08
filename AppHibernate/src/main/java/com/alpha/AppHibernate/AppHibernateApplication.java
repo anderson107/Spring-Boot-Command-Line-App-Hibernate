fb7e4ebe4f09249757bfe85a49e0d351628413c9
Anderson107 <anderson.gomes107@gmail.com>
2021-03-08 15:04:54 +0000

    Project structure

---
+++ AppHibernate/src/main/java/com/alpha/AppHibernate/AppHibernateApplication.java
@@ -0,0 +1,17 @@
+package com.alpha.AppHibernate;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AppHibernateApplication {
+	
+	public AppHibernateApplication() {
+		new Application();
+	}
+
+	public static void main(String[] args) {
+		SpringApplication.run(AppHibernateApplication.class, args);
+	}
+
+}
---
+++ AppHibernate/src/test/java/com/alpha/AppHibernate/AppHibernateApplicationTests.java
@@ -0,0 +1,13 @@
+package com.alpha.AppHibernate;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AppHibernateApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
