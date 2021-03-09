e02b75f65a948fd530540e4467bf0cce59c64439
Anderson107 <anderson.gomes107@gmail.com>
2021-03-09 14:21:06 +0000

    Hibernate java configuration added

--- AppHibernate/src/main/java/com/alpha/AppHibernate/AppHibernateApplication.java
+++ AppHibernate/src/main/java/com/alpha/AppHibernate/AppHibernateApplication.java
@@ -7,7 +7,7 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 public class AppHibernateApplication {
 	
 	public AppHibernateApplication() {
-		new Application();
+		new Application().displayMenu();
 	}
 
 	public static void main(String[] args) {
--- AppHibernate/src/main/java/com/alpha/Config/DbConfig.java
+++ AppHibernate/src/main/java/com/alpha/Config/DbConfig.java
@@ -1,23 +1,32 @@
 package com.alpha.Config;
 
+import java.io.IOException;
+import java.util.Properties;
+
 import javax.sql.DataSource;
 
+import org.hibernate.SessionFactory;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.ComponentScan;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.context.annotation.Lazy;
+import org.springframework.orm.hibernate5.HibernateTransactionManager;
+import org.springframework.orm.hibernate5.LocalSessionFactoryBean;
+import org.springframework.transaction.PlatformTransactionManager;
+import org.springframework.transaction.annotation.EnableTransactionManagement;
 
 import com.alpha.dao.*;
 
 @Configuration
 @ComponentScan(basePackages = "com.alpha.dao")
+@EnableTransactionManagement
 public class DbConfig {
 
 	@Autowired
 	private DbConnection dataSource;
 
-	@Bean(destroyMethod="close")
+	@Bean
 	public DataSource dataSource() {
 		return dataSource.getConnection();
 	}
@@ -37,5 +46,33 @@ public class DbConfig {
 		card.setDataSource(dataSource());
 		return card;
 	}
+	
+	private Properties hibernateProperties() {
+		Properties hibernateProp = new Properties();
+		hibernateProp.put("hibernate.dialect", "org.hibernate.dialect.MySQLDialect");
+		hibernateProp.put("hibernate.format_sql", true);
+		hibernateProp.put("hibernate.use_sql_comments", true);
+		hibernateProp.put("hibernate.show_sql", true);
+		hibernateProp.put("hibernate.max_fetch_depth", 3);
+		hibernateProp.put("hibernate.jdbc.batch_size", 10);
+		hibernateProp.put("hibernate.jdbc.fetch_size", 50);
+		
+		return hibernateProp;
+	}
 
+	@Bean
+	public SessionFactory sessionFactory() throws IOException {
+		LocalSessionFactoryBean session = new LocalSessionFactoryBean();
+		session.setDataSource(dataSource());
+		session.setPackagesToScan("com.alpha");
+		session.setHibernateProperties(hibernateProperties());
+		session.afterPropertiesSet();
+		
+		return session.getObject();
+	}
+	
+	@Bean
+	public PlatformTransactionManager transactionManager() throws IOException {
+		return new HibernateTransactionManager(sessionFactory());
+	}
 }
